6f33318fe72421e9946345970c7d20d65f915fcf
Rachelz85 <100074061+Rachelz85@users.noreply.github.com>
2022-04-23 18:47:18 +0300

    Added the search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
+import Dictionary from "./Dictionary"
 import './App.css';
 
 function App() {
   return (
+      <div className="container">
     <div className="App">
       <header className="App-header">
      <h1>Dictionary Project</h1>
-
+  
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>
+        Codded by Rachel Zilberstein
+        </small>
+      </footer>
+    </div>
     </div>
   );
 }
